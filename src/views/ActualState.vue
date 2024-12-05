df51eec6df6f007e9a2707887c3d1da06fdcdf9d
Eliana Montiel <elianamontiel_10@hotmail.com>
2024-12-05 10:44:31 -0300

    Eliminacion de codigo comentado

--- src/views/ActualState.vue
+++ src/views/ActualState.vue
@@ -1,103 +1,4 @@
- <!-- <template>
-    <div class="princ-container">
-      <h2>Estado Actual</h2>
-      <div class="container">
-          <table>
-          <thead>
-            <tr>
-              <th>Criptomoneda</th>
-              <th>Cantidad</th>
-              <th>Valor en Dinero</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(transaction, index) in transactions" :key="index">
-              <td>{{ transaction.crypto_code }}</td>
-              <td>{{ transaction.crypto_amount }}</td>
-              <td>{{formatMoney(transaction.money)  }}</td>
-            </tr>
-          </tbody>
-        </table>
-        <div>
-          <strong>Monto Total:</strong> {{ formatMoney(totalMoney ) }}
-        </div>
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  import CryptoYaApi from '@/services/CryptoYaApi';
-  
-  export default {
-    name: "ActualState",
-    data() {
-      return {
-        transactions: [],
-        cryptoState: [],
-        totalMoney: 0,
-      };
-    },
-    computed: {
-      calculateTotalMoney() {
-        return this.transactions.reduce((total, transaction) => total + transaction.money, 0);
-      },
-    },
-    methods: {
-      async fetchTransactions() {
-        try {
-          await this.$store.dispatch("fetchTransactions");
-          this.transactions = this.$store.state.transactions;
-          this.calculateCryptoState();
-        } catch (error) {
-          console.error("Error al obtener las transacciones:", error);
-        }
-      },
-      async calculateCryptoState() {
-        const cryptoMap = {};
-        this.transactions.forEach((transaction) => {
-          const { crypto_code, crypto_amount, action } = transaction;
-          if (!cryptoMap[crypto_code]) {
-            cryptoMap[crypto_code] = 0;
-          }
-      
-          cryptoMap[crypto_code] += action === "purchase" ? crypto_amount : -crypto_amount;
-        });
-  
-        const cryptoState = [];
-        for (const cryptoType in cryptoMap) {
-          const amount = cryptoMap[cryptoType];
-          if (amount > 0) {
-            try {
-              const response = await CryptoYaApi.getPrice(cryptoType, amount);
-              const money = response.data.totalBid || 0; 
-              cryptoState.push({ cryptoType, amount, money });
-            } catch (error) {
-              console.error(`Error al obtener el precio de ${cryptoType}:`, error);
-              cryptoState.push({ cryptoType, amount, money: 0 });
-            }
-          }
-        }
-  
-        this.cryptoState = cryptoState;
-        this.totalMoney = this.calculateTotalMoney;
-      },
-      formatMoney(value) {
-      const number = Number(value);
-      if (isNaN(number)) {
-        return value; 
-      }
-      return new Intl.NumberFormat("es-AR", {
-        style: "currency",
-        currency: "ARS",
-      }).format(number); 
-    },
-    },
-    async created() {
-      await this.fetchTransactions();
-    },
-  };
-  </script>    -->
-  <template>
+<template>
     <div class="container">
       <h2>Estado Actual de Finanzas</h2>
       <table>
@@ -120,9 +21,9 @@
         <strong>Monto Total:</strong> {{ formatMoney(totalMoney) }}
       </div>
     </div>
-  </template>
+</template>
   
-  <script>
+<script>
   import LaboratorioApi from "@/services/LaboratorioApi";
   
   export default {
@@ -138,7 +39,7 @@
       async fetchTransactions() {
         try {
           if (!this.userId) {
-            console.error("No se encontró el userId. Asegúrate de que el usuario esté autenticado.");
+            console.error("No se encontró el userId");
             return;
           }
   
@@ -198,10 +99,9 @@
       }
     },
   };
-  </script>
+</script>
   
-
-  <style scoped>
+<style scoped>
   .container{
       display: grid;
       justify-content: center;
@@ -221,5 +121,5 @@
     .total{
       color: aliceblue;
     }
-  </style>
+</style>
     
